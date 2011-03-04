236555d52603c5a1841394a563244a43dc0fc4f1
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-03-04 14:06:34 -0800

    New maven eclipse project

---
+++ src/main/java/net/avh4/util/imagecomparison/App.java
@@ -0,0 +1,13 @@
+package net.avh4.util.imagecomparison;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
