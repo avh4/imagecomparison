avh4/imagecomparison

commit 0624ccaea601ef608bb6d9120dc794960996dbe4

Write failed approval images to the current directory

imagecomparison/src/main/java/net/avh4/util/imagecomparison/ImageComparison.java
@@ -54,8 +54,9 @@ public class ImageComparison {
 	}
 
 	private static void write(final BufferedImage image, final String filename) {
+		final String safeFilename = filename.replaceFirst("^.*/*", "");
 		try {
-			ImageIO.write(image, "png", new File(filename));
+			ImageIO.write(image, "png", new File(safeFilename));
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}

commit e0355f77ca1cc8b84c64e56ba83ee5185df9b611

Fix for Write failed approval images to the current directory
A mistake in commit 0624ccaea601ef608bb6d9120dc794960996dbe4 caused
expected filenames without any slashes (such as those generated by
isApproved()) to cause a NullPointerException.

imagecomparison/src/main/java/net/avh4/util/imagecomparison/ImageComparison.java
@@ -54,7 +54,7 @@ public class ImageComparison {
 	}
 
 	private static void write(final BufferedImage image, final String filename) {
-		final String safeFilename = filename.replaceFirst("^.*/*", "");
+		final String safeFilename = filename.replaceFirst("^.*/+", "");
 		try {
 			ImageIO.write(image, "png", new File(safeFilename));
 		} catch (final IOException e) {